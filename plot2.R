de076a903b7242f8743640ec93473c1363bc3562
salvadorjp63 <jpsalvadorm@briza-soft.com>
2015-12-13 14:25:36 -0500

    days showing !

--- plot2.R
+++ plot2.R
@@ -1,8 +1,9 @@
 ## I have already filtered the necesary data for two days
+## the filter file is "household_power_consumption.csv"
+## day names in spanish ! . . . sorry for this
 
 pc <- read.csv("household_power_consumption.csv", header=TRUE, sep=";", na.string="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-f <- factor(pc$Date, labels=c("Thu", "Fri"))
-plot(pc$Global_active_power, ylab="Global active power (kilowatts)", type="l")
-## axis(at=f, side=1)
+pc <- within(pc, { timestamp=paste(format(as.Date(Date,format("%d/%m/%Y")),"%Y-%m-%d"),"T",Time, sep="") })
+plot(strptime(pc$timestamp, "%Y-%m-%dT%H:%M:%S"),pc$Global_active_power, ylab="Global active power (kilowatts)", type="l")
 dev.copy(png, file="plot2.png")
 dev.off()
