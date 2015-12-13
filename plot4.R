1db20bf31b55ebf1e895275bdf66df2af5fd8ed0
salvadorjp63 <jpsalvadorm@briza-soft.com>
2015-12-13 14:36:02 -0500

    showing days

--- plot4.R
+++ plot4.R
@@ -3,18 +3,20 @@
 ## this repository
 
 pc <- read.csv("household_power_consumption.csv", header=TRUE, sep=";", na.string="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+pc <- within(pc, { timestamp=paste(format(as.Date(Date,format("%d/%m/%Y")),"%Y-%m-%d"),"T",Time, sep="") })
+
 par(mfrow = c(2,2))
 
-plot(pc$Global_active_power, ylab="Global active power (kilowatts)", type="l")
+plot(strptime(pc$timestamp, "%Y-%m-%dT%H:%M:%S"),pc$Global_active_power, ylab="Global active power (kilowatts)", type="l")
 
-plot(pc$Voltage, ylab="Voltage", type="l")
+plot(strptime(pc$timestamp, "%Y-%m-%dT%H:%M:%S"),pc$Voltage, ylab="Voltage", type="l")
 
-plot(pc$Sub_metering_1, ylab="Energy sub metering", type="l", col='red')
+plot(strptime(pc$timestamp, "%Y-%m-%dT%H:%M:%S"),pc$Sub_metering_1, ylab="Energy sub metering", type="l", col='red')
 lines(pc$Sub_metering_2, col='green')
 lines(pc$Sub_metering_3, col='blue')
 legend("topright",c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("red","green","blue"))
 
-plot(pc$Global_reactive_power, ylab="Global reactive power", type="l", col='black')
+plot(strptime(pc$timestamp, "%Y-%m-%dT%H:%M:%S"),pc$Global_reactive_power, ylab="Global reactive power", type="l", col='black')
 
 dev.copy(png, file="plot4.png")
 dev.off()
