a9bb41f40c965d437dd66e4a930bb9624fe10a70
salvadorjp63 <jpsalvadorm@briza-soft.com>
2015-12-13 10:36:04 -0500

    plot 2 !!!

--- plot2.R
+++ plot2.R
@@ -2,6 +2,6 @@
 
 pc <- read.csv("household_power_consumption.csv", header=TRUE, sep=";", na.string="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 f <- factor(pc$Date, labels=c("Thu", "Fri"))
-with(pc, plot(strptime(Time,format="%H:%M:%S"), Global_active_power, ylab="Global active power (kilowatts)", type="l"))
-## dev.copy(png, file="plot2.png")
-## dev.off()
+plot(pc$Global_active_power, ylab="Global active power (kilowatts)", type="l")
+dev.copy(png, file="plot2.png")
+dev.off()
