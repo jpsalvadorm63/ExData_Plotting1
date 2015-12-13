29fff7a839ec18e2cef59ec2656f25bf7316e958
salvadorjp63 <jpsalvadorm@briza-soft.com>
2015-12-13 14:32:28 -0500

    days showing

--- plot3.R
+++ plot3.R
@@ -1,7 +1,8 @@
 ## I have already filtered the necesary data for two days
 
 pc <- read.csv("household_power_consumption.csv", header=TRUE, sep=";", na.string="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-plot(pc$Sub_metering_1, ylab="Energy sub metering", type="l", col='red')
+pc <- within(pc, { timestamp=paste(format(as.Date(Date,format("%d/%m/%Y")),"%Y-%m-%d"),"T",Time, sep="") })
+plot(strptime(pc$timestamp, "%Y-%m-%dT%H:%M:%S"),pc$Sub_metering_1, ylab="Energy sub metering", type="l", col='red')
 lines(pc$Sub_metering_2, col='green')
 lines(pc$Sub_metering_3, col='blue')
 legend("topright",c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("red","green","blue"))
